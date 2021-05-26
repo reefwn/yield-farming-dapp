e8f2518bca476105ad56d5953f7b22ed1c67024f
Yossapol Witayanont <reef_3llen@hotmail.com>
2021-05-26 22:39:15 +0700

    add dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import {
+  createMuiTheme,
+  CssBaseline,
+  Grid,
+  Paper,
+  Switch,
+  ThemeProvider,
+} from "@material-ui/core";
 import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    },
+  });
+
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Paper style={{ height: "100vh" }}>
+        <Navbar />
+        <Grid container justify="flex-end">
+          <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+        </Grid>
+      </Paper>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
